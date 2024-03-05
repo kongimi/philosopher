869d5ea1586d9cf2546d4f95315bbc55bbaa12d1
= <niran.analas@gmail.com>
2024-03-05 14:04:32 +0700

    add check_argv

--- philo/handle_error.c
+++ philo/handle_error.c
@@ -6,7 +6,7 @@
 /*   By: npiyapan <npiyapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 16:17:59 by npiyapan          #+#    #+#             */
-/*   Updated: 2024/03/03 16:33:27 by npiyapan         ###   ########.fr       */
+/*   Updated: 2024/03/05 14:02:55 by npiyapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 void	handle_errors(char *error_msg)
 {
 	write(STDERR_FILENO, "Error: ", 7);
-	write(STDERR_FILENO, error_msg, strlen(error_msg));
+	int i;
+
+	i = 0;
+	while (error_msg[i])
+		i++;
+	write(STDERR_FILENO, error_msg, i);
 	write(STDERR_FILENO, "\n", 1);
 	exit(EXIT_FAILURE);
 }
--- philo/philo.c
+++ philo/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npiyapan <niran.analas@gmail.com>          +#+  +:+       +#+        */
+/*   By: npiyapan <npiyapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:11:51 by npiyapan          #+#    #+#             */
-/*   Updated: 2024/03/03 20:12:27 by npiyapan         ###   ########.fr       */
+/*   Updated: 2024/03/05 14:03:32 by npiyapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@
 int	check_argv(char **argv)
 {
 	int	i;
-	// int	j;
+	int	j;
 
 	i = 1;
 	while (argv[i])
 	{
-		printf("%s ", argv[i]);
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (1);
+			j++;
+		}
 		i++;
 	}
 	return (0);
@@ -29,8 +35,8 @@ int	check_argv(char **argv)
 int	main(int argc, char **argv)
 {
 	if (argc != 6)
-		handle_errors("Usage : num_philo die eat sleep eat2\n");
+		handle_errors("Usage : amount die eat sleep eat2\n");
 	if (check_argv(argv))
-		handle_errors("input must be number.\n");
+		handle_errors("Usage : input must be number.\n");
 	return (0);
 }
