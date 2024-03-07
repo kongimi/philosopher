b56e82b11d2115e6e8c82d2f1e9c4be52af9541c
= <niran.analas@gmail.com>
2024-03-07 17:06:14 +0700

    create pthread

--- philo/init_philo.c
+++ philo/init_philo.c
@@ -6,16 +6,64 @@
 /*   By: npiyapan <npiyapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 14:42:37 by npiyapan          #+#    #+#             */
-/*   Updated: 2024/03/05 14:51:58 by npiyapan         ###   ########.fr       */
+/*   Updated: 2024/03/07 17:05:27 by npiyapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/philo.h"
 
+void	*print_hello(void *arg)
+{
+	int	i;
+
+	i = 0;
+	(void)arg;
+	while (i++ < 100)
+	{
+		printf("Hello ");
+		usleep(1000000);
+	}
+	return (NULL);
+}
+
+void	*print_world(void *arg)
+{
+	int	i;
+
+	i = 0;
+	(void)arg;
+	while (i++ < 100)
+	{
+		printf("World\n");
+		usleep(1000000);
+	}
+	return (NULL);
+}
+
 void	init_philo(t_philo *data, char **argv)
 {
+	pthread_t	t1;
+	pthread_t	t2;
+
 	data->amount = ft_atoi(argv[1]);
 	data->time_die = ft_atoi(argv[2]);
 	data->time_eat = ft_atoi(argv[3]);
 	data->time_sleep = ft_atoi(argv[4]);
+	printf("amount = %d\n", data->amount);
+	printf("die = %d\n", data->time_die);
+	printf("eat = %d\n", data->time_eat);
+	printf("sleep = %d\n", data->time_sleep);
+	printf("stop = %d\n", data->stop);
+	if (data->amount <= 0 || data->amount > 200 ||\
+		data->time_die <= 0 || data->time_eat <= 0 ||\
+		data->time_sleep <= 0)
+		handle_errors("Input must greater than 0.");
+	if (pthread_create(&t1, NULL, print_hello, NULL))
+		handle_errors("Thread T1 error");
+	if (pthread_create(&t2, NULL, print_world, NULL))
+		handle_errors("Thread T2 error");
+	if (pthread_join(t1, NULL))
+		handle_errors("join T1 error");
+	if (pthread_join(t2, NULL))
+		handle_errors("join T2 error");
 }
--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: npiyapan <npiyapan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:11:51 by npiyapan          #+#    #+#             */
-/*   Updated: 2024/03/05 14:52:11 by npiyapan         ###   ########.fr       */
+/*   Updated: 2024/03/07 16:43:33 by npiyapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,18 @@ int	check_argv(char **argv)
 	return (0);
 }
 
-
-
 int	main(int argc, char **argv)
 {
 	t_philo	data;
 
 	if (argc != 5 && argc != 6)
-		handle_errors("Usage : amount_of_philo die eat sleep stop\n");
+		handle_errors("Usage : amount_of_philo die eat sleep time_to_stop\n");
 	if (check_argv(argv))
-		handle_errors("Usage : input must be number.\n");
+		handle_errors("Usage : input must be number or greather than 0.\n");
 	if (argc == 6)
 		data.stop = ft_atoi(argv[5]);
 	else
-		data.stop = 0;
+		data.stop = -1;
 	init_philo(&data, argv);
 	return (0);
 }
